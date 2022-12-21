10e0b051550d32328bb924edd68ba31b829bb85e
ranafge <ranafge@gmail.com>
2022-12-21 16:44:48 +0600

    Adding Custom commands for login

--- cypress/e2e/3-loginwith-csrftoken/loginWithCsrfToken.cy.js
+++ cypress/e2e/3-loginwith-csrftoken/loginWithCsrfToken.cy.js
@@ -5,6 +5,20 @@ describe('Login with -CsrfToken', function(){
     const password = Cypress.env('password');
     console.log(username, password)
 
+    Cypress.Commands.add('loginWithCsrfToken', (csrfToken) =>{
+        cy.request({url: 'http://quotes.toscrape.com/login',
+                    method: 'POST',
+                    form: true,
+                    body: {
+                        username,
+                        password,
+                        csrf_token: csrfToken
+                    }
+                }
+
+            )
+    })
+
 
     it('Parse from csrf token from HTML', function() {
         cy.request('http://quotes.toscrape.com/login')
@@ -13,6 +27,14 @@ describe('Login with -CsrfToken', function(){
             const $html = Cypress.$(body)
             const csrfToken = $html.find('form > input[type=hidden]:nth-child(1)').val();
             console.log(csrfToken)
+            cy.loginWithCsrfToken(csrfToken)
+            .then((res)=>{
+                expect(res.status).to.eql(200)
+                expect(res.body).to.include('Logout')
+                cy.visit(Cypress.env('baseUrl'))
+            })
+
+
           
         })
     })
